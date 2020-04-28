5e557bbbfc2de6a1192983ac326be602542c4549
joshua salzedo <thHunkn0WNd@gmail.com>
2020-04-28 13:55:39 -0700

    remove redundant mod declaration

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,26 @@
+use bitfield::bitfield;
 
-pub mod ls7366 {
-    use bitfield::bitfield;
+pub enum TargetRegister {
+    SelectMdr0,
+    SelectMdr1,
+    SelectDtr,
+    SelectCenter,
+    SelectOtr,
+    SelectStr,
+    SelectNone,
+}
 
-    pub enum TargetRegister {
-        SelectMdr0,
-        SelectMdr1,
-        SelectDtr,
-        SelectCenter,
-        SelectOtr,
-        SelectStr,
-        SelectNone,
-    }
+pub enum RegisterAction {
+    ClearRegister,
+    ReadRegister,
+    WriteRegister,
+    LoadRegister,
+}
 
-    pub enum RegisterAction {
-        ClearRegister,
-        ReadRegister,
-        WriteRegister,
-        LoadRegister,
-    }
-
-    bitfield! {
+bitfield! {
     pub struct RawMessage(u8);
     impl Debug;
     pub discarded, set_discarded: 0,2;
     pub selected_register, set_selected_register: 3,5;
     pub selected_action, set_selected_action: 6,7;
     }
-}
