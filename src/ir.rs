6059dece73e3f4ba584742c28460faaace678ea5
joshua salzedo <thHunkn0WNd@gmail.com>
2020-04-28 20:05:15 -0700

    Implemented decode operations for the Instruction Register

--- src/ir.rs
+++ src/ir.rs
@@ -1,6 +1,7 @@
 use bitfield::bitfield;
 
-use crate::traits::Encodable;
+use crate::errors::DecodeError;
+use crate::traits::{Decodable, Encodable};
 
 #[derive(Debug)]
 pub enum Target {
@@ -37,13 +38,28 @@ bitfield! {
 impl Encodable for Target {
     fn encode(&self) -> u8 {
         match self {
-            Target::Mdr0 => { 0b001 }
-            Target::Mdr1 => { 0b010 }
-            Target::Dtr => { 0b011 }
-            Target::Center => { 0b100 }
-            Target::Otr => { 0b101 }
-            Target::Str => { 0b110 }
-            Target::None => { 0b111 }
+            Target::Mdr0 => 0b001,
+            Target::Mdr1 => 0b010,
+            Target::Dtr => 0b011,
+            Target::Center => 0b100,
+            Target::Otr => 0b101,
+            Target::Str => 0b110,
+            Target::None => 0b111,
+        }
+    }
+}
+
+impl Decodable for Target {
+    fn decode(raw: u8) -> Result<Self, DecodeError> {
+        match raw {
+            0b001 => Ok(Target::Mdr0),
+            0b010 => Ok(Target::Mdr1),
+            0b011 => Ok(Target::Dtr),
+            0b100 => Ok(Target::Center),
+            0b101 => Ok(Target::Otr),
+            0b110 => Ok(Target::Str),
+            0b111 => Ok(Target::None),
+            _ => { Err(DecodeError::Failed("failed to decode Target".to_string())) }
         }
     }
 }
@@ -51,10 +67,22 @@ impl Encodable for Target {
 impl Encodable for Action {
     fn encode(&self) -> u8 {
         match self {
-            Action::Clear => { 0b00 }
-            Action::Read => { 0b01 }
-            Action::Write => { 0b10 }
-            Action::Load => { 0b11 }
+            Action::Clear => 0b00,
+            Action::Read => 0b01,
+            Action::Write => 0b10,
+            Action::Load => 0b11,
+        }
+    }
+}
+
+impl Decodable for Action {
+    fn decode(raw: u8) -> Result<Self, DecodeError> {
+        match raw {
+            0b00 => Ok(Action::Clear),
+            0b01 => Ok(Action::Read),
+            0b10 => Ok(Action::Write),
+            0b11 => Ok(Action::Load),
+            _ => Err(DecodeError::Failed("failed to decode Action".to_string()))
         }
     }
 }
@@ -65,5 +93,15 @@ impl Encodable for InstructionRegister {
         payload.set_target(self.target.encode());
         payload.set_action(self.action.encode());
         payload.0
+    }
+}
+
+impl Decodable for InstructionRegister {
+    fn decode(raw: u8) -> Result<Self, DecodeError> {
+        let payload = Payload(raw);
+        Ok(Self {
+            target: Target::decode(payload.target())?,
+            action: Action::decode(payload.action())?,
+        })
     }
 }
