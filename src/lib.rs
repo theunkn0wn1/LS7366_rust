9c88fba232338610a4b55916a2561c1c059c5a4b
joshua salzedo <thHunkn0WNd@gmail.com>
2020-04-30 13:49:09 -0700

    replicated examples/quickstart.rs in module documentation  - moved rpppal to a dev dependency as any HAL SPI *should* work

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,31 @@
 //!
 //! # Examples
 //! ```no_run
-//! use ls7366::Ls7366;
-//! use rppal::spi::Spi;
-//! fn your_code(spi_interface: Spi){
-//!     // Create an instance of the driver from some HAL SPI implementation
-//!     let mut driver = Ls7366::new(spi_interface).unwrap();
-//!     // Initialize the chip with a sensible default configuration.
-//! }
+//!   use ls7366::Ls7366;
+//! // --- snip ---
+//! # use std::error::Error;
+//! #
+//! # use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
+//! #
+//! # use std::thread::sleep;
+//! # use std::time::Duration;
+//! # fn main() -> Result<(), Box<dyn Error>> {
+//! #    // create an instance of an SPI object
+//! #    // In this case, the buffer is on SPI0 and SS1.
+//! #    // The chip acts in Mode0.
+//! #    let some_hal_spi_object = Spi::new(Bus::Spi0, SlaveSelect::Ss1, 14_000_000, Mode::Mode0)?;
+//! #
+//!     // Construct a driver instance from the SPI interface, using default chip configurations.
+//!     let mut spi_driver = Ls7366::new(some_hal_spi_object)?;
+//!
+//!     // Loop and read the counter.
+//!     loop {
+//!         let result = spi_driver.get_count()?;
+//!         sleep(Duration::from_secs(1));
+//!         println!("read data:= {:?}", result);
+//!     }
+//! // --- snip ---
+//! # }
 //! ```
 //!
 use embedded_hal::blocking::spi::{Transfer, Write};
