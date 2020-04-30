0217de12ea7e619a16966a9bcc5a6fa8852109a2
joshua salzedo <thHunkn0WNd@gmail.com>
2020-04-30 13:19:59 -0700

    Santity check in the bytes converter

--- examples/quickstart.rs
+++ examples/quickstart.rs
@@ -3,21 +3,22 @@ use std::error::Error;
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
 
 use ls7366::Ls7366;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() -> Result<(), Box<dyn Error>> {
-//    let spi_0 = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 14000000, Mode::Mode0)?;
     // create an instance of an SPI object
     // In this case, the buffer is on SPI0 and SS1.
     // The chip acts in Mode0.
     let spi_1 = Spi::new(Bus::Spi0, SlaveSelect::Ss1, 14_000_000, Mode::Mode0)?;
 
-    // Hand the SPI interface over to the driver.
+    // Construct a driver instance from the SPI interface, using default chip configurations.
     let mut spi_driver = Ls7366::new(spi_1)?;
-
 
     // Loop and read the counter.
     loop {
         let result = spi_driver.get_count()?;
+        sleep(Duration::from_secs(1));
         println!("read data:= {:?}", result);
     }
 }
--- src/utilities.rs
+++ src/utilities.rs
@@ -1,5 +1,8 @@
 /// Converts a vector of length 4 into a u32
-pub(crate) fn vec_to_u32(data: &Vec<u8>) -> u32 {
+pub(crate) fn vec_to_u32(data: &[u8]) -> u32 {
+    if data.len()>4{
+        panic!("payload too big!, got {:?}", data)
+    }
     let mut result: u32 = 0x00;
     let mut i = 0;
     for byte in data.iter().rev() {
@@ -12,6 +15,6 @@ pub(crate) fn vec_to_u32(data: &Vec<u8>) -> u32 {
 }
 #[test]
 fn test_vec_to_u32(){
-    let result = vec_to_u32(&vec![0xDE, 0xAD, 0xBE, 0xEF]);
-    assert_eq!(result, 0xDEADBEEF);
+    assert_eq!(vec_to_u32(&vec![0xDE, 0xAD, 0xBE, 0xEF]), 0xDEADBEEF);
+    assert_eq!(vec_to_u32(&vec![0x00, 0x00, 0x00, 0x0]), 0x0000000)
 }
