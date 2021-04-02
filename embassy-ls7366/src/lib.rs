e3a85552c31cc39d01e662ea1069303c598879f6
Joshua Salzedo <joshuasalzedo@gmail.com>
2021-04-02 12:41:27 -0700

    implemented write_register

--- embassy-ls7366/src/lib.rs
+++ embassy-ls7366/src/lib.rs
@@ -2,18 +2,27 @@
 
 use embassy_traits::spi::FullDuplex;
 
-use ls7366::{ir, mdr0, mdr1};
+use ls7366::{ir, mdr0, mdr1, Encodable};
+use ls7366::Error;
 
 use core::pin::Pin;
+use core::future::Future;
+
+
+pub mod error;
 
 struct Ls7366<SPI> {
     iface: SPI
 }
 
-impl<SPI> Ls7366<SPI>
-    where SPI: FullDuplex<u8> {
-    pub fn new(iface: SPI) -> Self {
-        let mut driver = Self { iface };
+impl<SPI, SpiError> Ls7366<SPI>
+    where SPI: FullDuplex<u8, Error=SpiError> {
+
+    pub fn new_uninit(iface: SPI) -> Self {
+        Self { iface }
+    }
+
+    async fn init<'a>(self: Pin<&'a mut Self>) -> Result<(), Error<SpiError>>{
         // Creating configurations for the two MDR configuration registers
         let mdr0_payload = mdr0::Mdr0 {
             quad_count_mode: mdr0::QuadCountMode::Quad4x,
@@ -30,15 +39,46 @@ impl<SPI> Ls7366<SPI>
             flag_on_bw: false,
             flag_on_cy: false,
         };
-        driver
+
+        self.write_register(ir::Target::Mdr0, &[mdr0_payload.encode()]).await?;
+        Ok(())
     }
 
 
-    async fn write_register<'a>(self: Pin<&'a mut Self>, target: ir::Target, data: &'a [u8]) {
-        self.iface.write(data).await?;
+    /// Writes data to specified register
+    ///
+    /// NOTE(safety) safe as Self is pinned.
+    async fn write_register<'a>(mut self: Pin<&'a mut Self>, target: ir::Target, data: &'a [u8]) -> Result<(), Error<SpiError>>{
+
+        let ir_cmd = ir::InstructionRegister {
+            target,
+            action: ir::Action::Write,
+        };
+        if data.len() > 4 {
+            return Err(Error::PayloadTooBig);
+        }
+
+        let encoded = ir_cmd.encode();
+        let payload: &mut [u8] = &mut [encoded, encoded, encoded, encoded, encoded];
+
+        let mut i = 1;
+        for datum in data {
+            payload[i] = *datum;
+            i+=1;
+        }
+
+        let result = unsafe{
+             Pin::new_unchecked(&mut self.get_unchecked_mut().iface).write(data).await
+        };
+        match result {
+            Ok(()) =>{
+                Ok(())
+            }
+            Err(e) => {
+                Err(Error::SpiError(e))
+            }
+        }
     }
-
-
 }
 
 #[cfg(test)]
